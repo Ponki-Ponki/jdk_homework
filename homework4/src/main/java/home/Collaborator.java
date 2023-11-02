8b30bc9eb332210f09f0fba12bdacd91ef57fef3
Юрий Мусихин <vensop@gmail.com>
2023-11-02 13:08:47 +0300

    update

---
+++ homework4/src/main/java/home/Collaborator.java
@@ -0,0 +1,37 @@
+package home;
+
+public class Collaborator {
+    Integer tabNumber;
+    String phone;
+    String name;
+    Integer ageWork;
+
+    public Collaborator(Integer id, String phone, String name, Integer ageWork) {
+        this.tabNumber = id;
+        this.phone = phone;
+        this.name = name;
+        this.ageWork = ageWork;
+    }
+    public Collaborator(Integer id) {
+        this.tabNumber = id;
+        this.phone = "89990000000";
+        this.name = "Ivan";
+        this.ageWork = 10;
+    }
+
+    public Integer getTabNumber() {
+        return tabNumber;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAgeWork() {
+        return ageWork;
+    }
+}
--- homework4/src/main/java/home/ListCollaborator.java
+++ homework4/src/main/java/home/ListCollaborator.java
@@ -1,4 +1,46 @@
 package home;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListCollaborator {
+    List<Collaborator> list;
+
+    public ListCollaborator() {
+        this.list = new ArrayList<>();
+    }
+
+    public void newCollaborator(){
+        Integer id = 0;
+        if (!list.isEmpty()) id = list.get(list.size()-1).getTabNumber();
+        Collaborator temp = new Collaborator(id);
+        list.add(temp);
+    }
+
+    public List<Collaborator> findCollaboratorAge(Integer age){
+        List<Collaborator> answer = new ArrayList<>();
+        for (Collaborator item : list) {
+            if (item.getAgeWork().equals(age)) answer.add(item);
+        }
+        return  answer;
+    }
+
+    public String  getPhoneNumber(String name){
+        String phone = "None";
+        for (Collaborator item : list) {
+            if (item.getName().equals(name)) phone = item.getPhone();
+        }
+        return phone;
+    }
+
+    public void printNumberPhoneByName(String name){
+        System.out.println(getPhoneNumber(name));
+    }
+
+    public Collaborator findCollaborator(Integer tabNumber){
+        for (Collaborator item : list) {
+            if (item.getTabNumber().equals(tabNumber)) return item;
+        }
+        return ;
+    }
 }
